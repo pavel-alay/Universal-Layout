8de83bb3eedc1979efacbb0f244b0f2e1e78e215
Pavel Alay <pavel.alay@gmail.com>
2021-10-10 09:57:55 +0300

    Add Colemak

---
+++ build-colemak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+
+iconutil --convert icns --output Universal.bundle/Contents/Resources/English\ -\ Universal.icns English-light.iconset
+iconutil --convert icns --output Universal.bundle/Contents/Resources/English\ -\ Colemak.icns English.iconset
+iconutil --convert icns --output Universal.bundle/Contents/Resources/Russian\ -\ Universal.icns Russian.iconset
+
+touch Universal.bundle
+
+sudo rm -rf /Library/Keyboard\ Layouts/Universal.bundle
+sudo cp -r Universal.bundle /Library/Keyboard\ Layouts/
